b746fc2efc5edb5656f0a6f4e9bee957533d1800
makka.kim <aa019@hotmail.com>
2023-04-14 23:59:44 +0900

    added class filter & batch skill

--- src/app/rank/list.tsx
+++ src/app/rank/list.tsx
@@ -14,6 +14,38 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
+const POEHOST = 'https://poe.game.daum.net/'
+// const POEHOST = 'https://www.pathofexile.com/'
+
+const CLASS:any = {
+  Marauder:'https://i.imgur.com/UWvvGKZ.png',
+  Duelist:'https://i.imgur.com/c8vB7OX.png',
+  Ranger:'https://i.imgur.com/qtdp8Jh.png',
+  Shadow:'https://i.imgur.com/nWahbNR.png',
+  Witch:'https://i.imgur.com/XSqMHh9.png',
+  Templar:'https://i.imgur.com/i53a1id.png',
+  Scion:'https://i.imgur.com/9IfOWoN.png',
+  Juggernaut:'https://i.imgur.com/QFubXr4.png',
+  Berserker:'https://i.imgur.com/l2vDx4j.png',
+  Chieftain:'https://i.imgur.com/WSkz5xh.png',
+  Slayer:'https://i.imgur.com/UMAd0yL.png',
+  Gladiator:'https://i.imgur.com/F3FQxV4.png',
+  Champion:'https://i.imgur.com/ltGrJ1K.png',
+  Deadeye:'https://i.imgur.com/NCybIiO.png',
+  Raider:'https://i.imgur.com/oFwpUJO.png',
+  Pathfinder:'https://i.imgur.com/EVg7lhR.png',
+  Assassin:'https://i.imgur.com/0tURSJ4.png',
+  Saboteur:'https://i.imgur.com/7dNJPM4.png',
+  Trickster:'https://i.imgur.com/n8jtbfr.png',
+  Necromancer:'https://i.imgur.com/k1debpx.png',
+  Occultist:'https://i.imgur.com/BGrQsx5.png',
+  Elementalist:'https://i.imgur.com/G5fSIS8.png',
+  Inquisitor:'https://i.imgur.com/PyzEPzP.png',
+  Hierophant:'https://i.imgur.com/8iu1k86.png',
+  Guardian:'https://i.imgur.com/sHiE02Y.png',
+  Ascendant:'https://i.imgur.com/Th9qGrm.png'
+}
+
 export default function Page(props:any) {
     const {userData} = props
   const [original, setOriginal] = useState<any[]>(userData)
@@ -21,6 +53,7 @@ export default function Page(props:any) {
   const [gemList, setGemList] = useState<any[]>([])
   const [uniqueList, setUniqueList] = useState<any[]>([])
   
+  const [filterClass, setClass] = useState<string>('')
   const [filterName, setName] = useState<string>('')
   const [filterLink, setLink] = useState<string>('')
   
@@ -63,7 +96,7 @@ export default function Page(props:any) {
   }
 
   useEffect(()=>{
-    if (filterName==='' && (filterGem===''||filterGem===null) && filterDeath==='all' && filterUnique==='' && filterLink===''){
+    if (filterName==='' && (filterGem===''||filterGem===null) && filterDeath==='all' && filterUnique==='' && filterLink==='' && filterClass===''){
       setFilter(original)
       return
     }else{
@@ -71,6 +104,7 @@ export default function Page(props:any) {
         const gemCheck = filterGem ? user.items?.allGems?.findIndex((gem:any)=>gem===filterGem)>-1 : true
         const uniqueCheck = filterUnique ? user.items?.allUniques?.findIndex((unique:any)=>unique===filterUnique)>-1 : true
         const deathCheck = filterDeath === 'all'? true : filterDeath==='dead'?user.dead:!user.dead
+        const classCheck = filterClass ? filterClass===user.class :true
         const nameCheck = filterName ? (user.name.includes(filterName) || user.account?.includes(filterName) || user.class.includes(filterName)) : true
         const linkCheck = filterLink ? (
           filterLink==='6'? user.items?.has6Link : (
@@ -79,7 +113,7 @@ export default function Page(props:any) {
             )
           )
         ): true
-        return Boolean(gemCheck&&nameCheck&&deathCheck&&uniqueCheck&&linkCheck)
+        return Boolean(gemCheck&&nameCheck&&deathCheck&&uniqueCheck&&linkCheck&&classCheck)
       }).sort((a,b)=>{
         return a.rank-b.rank
       })
@@ -87,16 +121,15 @@ export default function Page(props:any) {
     }
 
     
-  },[filterGem, filterName, filterDeath, filterUnique,filterLink])
+  },[filterGem, filterName, filterDeath, filterUnique,filterLink, filterClass])
   
   const handleChange = (event: SelectChangeEvent) => {
     setLink(event.target.value as string);
   };
 
-
   return <TableContainer component={Paper} style={{minWidth:335}}>
     <div className="search" style={{margin:20}}>
-    <TextField id="outlined-basic" label="검색" variant="outlined" onChange={findName} />
+    <TextField style={{minWidth:150}} id="outlined-basic" label="검색" variant="outlined" onChange={findName} />
     <Autocomplete
       disablePortal
       id="combo-box-gem"
@@ -127,7 +160,7 @@ export default function Page(props:any) {
         }}
       renderInput={(params) => <TextField {...params} label="유닠" />}
     />
-    <FormControl style={{width:150}}>
+    <FormControl style={{minWidth:150}}>
         <InputLabel id="demo-simple-select-label">링크</InputLabel>
         <Select
           labelId="demo-simple-select-label"
@@ -148,6 +181,11 @@ export default function Page(props:any) {
         <Button style={{backgroundColor:filterDeath==='alive'?'#5e51af':'gray'}} onClick={()=>setDeath('alive')}>덜죽음</Button>
       </ButtonGroup>
       </div>
+      <div className='classes'>{Object.keys(CLASS).map(className=>{
+        return <div onClick={()=>setClass(filterClass===className?'':className)} style={{border:filterClass===className?'3px solid #50dd33':'none'}} className='classBox'>
+          <img src={CLASS[className]} />
+        </div>
+      })}</div>
       <Table //aria-label="simple table" 
       className="text-left text-sm font-light">
         <TableHead className="border-b font-medium dark:border-neutral-500">
@@ -194,7 +232,7 @@ export default function Page(props:any) {
                 <a style={{    backgroundColor: 'white',
     display: 'inline-block',
     padding: '2px 10px',
-    borderRadius: 5}} target='_blank' href={`https://www.pathofexile.com/account/view-profile/${row.account}/characters?characterName=${row.name}`}>
+    borderRadius: 5}} target='_blank' href={`${POEHOST}account/view-profile/${row.account}/characters?characterName=${row.name}`}>
                   POE
                 </a>
               </TableCell>
