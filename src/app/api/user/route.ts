5c438c1afc622e297af315610c499a2d722d6624
makka.kim <aa019@hotmail.com>
2023-08-13 17:54:00 +0900

    remove api cache

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import getUsers from './controller'
 
-export const revalidate = 10
+export const revalidate = 0
 
 export async function GET(request: Request, param:any) {
   const userData = await getUsers()
