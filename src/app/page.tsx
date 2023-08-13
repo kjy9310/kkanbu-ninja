8e8e5e60935fe133e5c5fc49f785eb216a42e9ce
makka.kim <aa019@hotmail.com>
2023-08-13 16:26:16 +0900

    added console for debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import RankList from './rank/list'
 
-async function getData() {
+async function getUserData() {
   try{
-    const res = await fetch(`${process.env.host}/api/user`,{ next: { revalidate: 10 } }); //10 min cache
+    const res = await fetch(`${process.env.host}/api/user`,{ next: { revalidate: 0 } }); //10 min cache
   
-  if (!res.ok) {
-    console.log('에러낫다! 스샷찍어주실?')
-    console.log(res)
-    return []
-  }
-  
-  return res.json();
+    if (!res.ok) {
+      console.log('에러낫다! 스샷찍어주실?')
+      console.log(res)
+      return []
+    }
+
+    return res.json();
   } catch(e){
     console.log('error:', e)
     return []
   }
-  
 }
 
 export default async function Page() {
-    const data = await getData()
+  console.log('before getUserData ')
+    const data = await getUserData()
+    console.log('after getUserData ')
     return <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <RankList userData={data||[]}/>
   </main>
