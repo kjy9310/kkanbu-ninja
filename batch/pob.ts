7521b391b6f595252b3e17bb6ea4568a07d1a9ff
makka <makka@DESKTOP-OLOE990.>
2025-06-14 23:26:08 +0900

    edit pob update time to 30min

--- batch/pob.ts
+++ batch/pob.ts
@@ -58,7 +58,7 @@ const batchMain = async () => {
     const dotenv = require('dotenv');
     dotenv.config();
 
-    const updateHourLimit = 1/4 * 60 // min
+    const updateHourLimit = 1/2 * 60 // min
 
     // Connection URL
     const client = new MongoClient(process.env.mongodb||'no db env');
